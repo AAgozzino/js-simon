2293f1c1d92d2f1bb4f88d5c4dc433de9cf303a0
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-04 15:12:15 +0200

    Scomposizione problema

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* Un alert espone 5 numeri casuali diversi.
+Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati. */
+
+// Genero numeri casuali
+
+// Inserisco 5 numeri in un array PC
+
+// Alert che mostra i 5 numeri casuali
+
+// Dopo 30 secondi prompt che chiede i 5 nuemri
+  // Inserire i numeri in array Utente
+
+// Messaggio con quanti numeri l'utente si è ricordato
+  // Per ogni numero inserito dall'utente controllo se è presente nell'array PC --> punteggio++
