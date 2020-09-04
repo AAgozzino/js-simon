dff70547ddf484397caaa25d8e8060ee7832d8be
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-04 15:34:33 +0200

    Prompt numeri utente

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha 
 Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati. */
 
 // Genero numeri casuali
+var min = 1;
+var max = 100;
 function randomNumber(min, max){
   return  Math.floor(Math.random()* (max-min + 1)) + min;
 };
 // Inserisco 5 numeri in un array PC
 var numeriPC = [];
 while (numeriPC.length < 5) {
-  let numeroRandom = randomNumber(1,100);
-  if (numeriPC.includes(numeroRandom) == false) {
-    numeriPC.push(numeroRandom)
+  let numRandom = randomNumber(min,max);
+  if (numeriPC.includes(numRandom) == false) {
+    numeriPC.push(numRandom)
   }
 };
 console.log(numeriPC);
@@ -19,6 +21,19 @@ console.log(numeriPC);
 alert(numeriPC);
 // Dopo 30 secondi prompt che chiede i 5 nuemri
   // Inserire i numeri in array Utente
+var numeriUtente = [];
+while (numeriUtente.length < 5) {
+  let numInserito = parseInt(prompt("Inserisci uno dei 5 numeri mostrati"))
+  if (numInserito < min || numInserito > max) {
+    alert("Attenzione!!! Numero non valido")
+  }
+  else if (numeriUtente.includes(numInserito)) {
+    alert("Attenzione!!! Numero già inserito")
+  } else {
+    numeriUtente.push(numInserito)
+  }
+};
 
+console.log(numeriUtente);
 // Messaggio con quanti numeri l'utente si è ricordato
   // Per ogni numero inserito dall'utente controllo se è presente nell'array PC --> punteggio++
