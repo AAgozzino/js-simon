257163c1c9251bee730db8b387b7ecb2a4c0ef3a
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-04 15:46:40 +0200

    Inserita setTimeout function

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati
 // Genero numeri casuali
 var min = 1;
 var max = 100;
+var delayTime = 5;
+
 function randomNumber(min, max){
   return  Math.floor(Math.random()* (max-min + 1)) + min;
 };
@@ -22,25 +24,28 @@ alert(numeriPC);
 // Dopo 30 secondi prompt che chiede i 5 nuemri
   // Inserire i numeri in array Utente
 var numeriUtente = [];
-while (numeriUtente.length < 5) {
-  let numInserito = parseInt(prompt("Inserisci uno dei 5 numeri mostrati"))
-  if (numInserito < min || numInserito > max) {
-    alert("Attenzione!!! Numero non valido")
-  }
-  else if (numeriUtente.includes(numInserito)) {
-    alert("Attenzione!!! Numero già inserito")
-  } else {
-    numeriUtente.push(numInserito)
-  }
-};
+setTimeout(function(){
+  while (numeriUtente.length < 5) {
+    let numInserito = parseInt(prompt("Inserisci uno dei 5 numeri mostrati"))
+    if (numInserito < min || numInserito > max) {
+      alert("Attenzione!!! Numero non valido")
+    }
+    else if (numeriUtente.includes(numInserito)) {
+      alert("Attenzione!!! Numero già inserito")
+    } else {
+      numeriUtente.push(numInserito)
+    }
+  };
+  console.log(numeriUtente);
+  var punteggio = 0;
+  for (var i = 0; i < numeriUtente.length; i++) {
+    if (numeriPC.includes(numeriUtente[i])) {
+      punteggio++;
+    }
+  };
+  console.log(punteggio);
+}, delayTime * 1000);
 
-console.log(numeriUtente);
+
 // Messaggio con quanti numeri l'utente si è ricordato
   // Per ogni numero inserito dall'utente controllo se è presente nell'array PC --> punteggio++
-var punteggio = 0;
-for (var i = 0; i < numeriUtente.length; i++) {
-  if (numeriPC.includes(numeriUtente[i])) {
-    punteggio++;
-  }
-};
-console.log(punteggio);
